4fe7f57687c4bdf754877205472beead9dbad0bd
Jim Wallace <jim@jet.com>
2015-02-06 11:24:42 -0500

    invoke Visual Studio environment when powershell launches

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,24 @@
-
-$mypaths = @(
+$paths = @(
     "C:\Program Files\Sublime Text 3"
+    "C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0"
+    "C:\Program Files (x86)\Git\bin"
 )
-$env:Path = [string]::Join(";", $mypaths) + ";" + $env:Path
+
+$env:Path = [string]::Join(";", $paths) + ";" + $env:Path
 
 # Load posh-git example profile
 . 'C:\tools\poshgit\dahlbyk-posh-git-869d4c5\profile.example.ps1'
 
 Rename-Item Function:\Prompt PoshGitPrompt -Force
 function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
+
+#Set environment variables for Visual Studio Command Prompt
+pushd 'c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
+cmd /c "vcvarsall.bat&set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+  }
+}
+popd
+write-host "`nVisual Studio 2010 Command Prompt variables set." -ForegroundColor Yellow
